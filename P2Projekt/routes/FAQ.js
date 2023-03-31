ebc9c343dc0cde4aeda3a20098a8fe12b4392658
NicklasKleemann <71381595+NicklasKleemann@users.noreply.github.com>
2023-03-31 11:28:30 +0200

    Added basis for FAQ page

--- P2Projekt/routes/FAQ.js
+++ P2Projekt/routes/FAQ.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET FAQ listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('FAQ', {title: "FAQ Page"})
 });
 
 module.exports = router;
