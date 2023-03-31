05792468f113ef746a01a4498872a7bff564419e
Thomass1412 <nuggi.lunoe@gmail.com>
2023-03-31 11:48:10 +0200

    captcha page added

--- P2Projekt/routes/captcha.js
+++ P2Projekt/routes/captcha.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'EHR JOURNAL HALLØJ' });
+    res.render('captcha', { title: 'captcha' });
 });
 
 module.exports = router;
