3ed7a98cabe36eafa36a215a58746e9272be1720
Thomass1412 <nuggi.lunoe@gmail.com>
2023-04-07 09:04:46 +0200

    Captcha implemented

--- P2Projekt/app.js
+++ P2Projekt/app.js
@@ -3,12 +3,45 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const { stringify } = require('querystring')
+var router = express.Router();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/FAQ');
 var captchaRouter = require('./routes/captcha');
 
 var app = express();
+
+app.use(express.json());
+
+app.post('/gethomepage', async (req, res) => {
+  if (!req.body.captcha)
+    return res.json({ success: false, msg: 'Please select captcha' });
+
+  // Secret key
+  const secretKey = '6LdpvDEUAAAAAHszsgB_nnal29BIKDsxwAqEbZzU';
+
+  // Verify URL
+  const query = stringify({
+    secret: secretKey,
+    response: req.body.captcha,
+    remoteip: req.connection.remoteAddress
+  });
+  const verifyURL = `https://google.com/recaptcha/api/siteverify?${query}`;
+
+  // Make a request to verifyURL
+  const body = await fetch(verifyURL).then(res => res.json());
+
+  // If not successful
+  if (body.success !== undefined && !body.success)
+    return res.json({ success: false, msg: 'Failed captcha verification' });
+
+  // If successful
+  return res.json({ success: true, msg: 'Captcha passed' });
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
