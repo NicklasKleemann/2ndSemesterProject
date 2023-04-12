0a8c41ed62f787af1ce9a52a5a0b5d6fc4cb19af
Thomass1412 <nuggi.lunoe@gmail.com>
2023-04-12 14:30:01 +0200

    added star review button and 'server'

--- P2Projekt/public/javascripts/server.js
+++ P2Projekt/public/javascripts/server.js
@@ -1,12 +1,14 @@
 let star_reviews = [];
 
-const btn = document.getElementById('button3');
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.getElementById('button3');
 
-btn.addEventListener("click", addStarReview);
+    btn.addEventListener("click", addStarReview);
 
-console.log(btn);
+    console.log(btn);
 
-function addStarReview() {
-    console.log(star_reviews);
-    star_reviews.push("5");
-}
+    function addStarReview() {
+        console.log(star_reviews);
+        star_reviews.push("5");
+    }
+});
