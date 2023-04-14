9e345288091df7cbaec6ec80543b1f25ef68a6ac
Imar <116631084+MrButtNugget@users.noreply.github.com>
2023-04-14 12:00:10 +0200

    added a TextPromt to AI function
    have yet to add all the API stuff
    it only reads inputs and prints output

--- P2Projekt/public/javascripts/script.js
+++ P2Projekt/public/javascripts/script.js
@@ -5,5 +5,19 @@ function Helloworld(){
 function outputText() {
     const inputText = document.getElementById("input-text").value;
     document.getElementById("output-area").innerText = inputText + " but it has been translated";
-  }
+}
+
+function TextPromptToAI(prompt) {
+    const AIprompt = prompt;
+    const inputText = document.getElementById("input-text").value;
+    const ToAIText = AIprompt + inputText;
+
+    // add API stuff here    
+
+
+
+    //prints the returned text from the API in a textbox on the website
+    document.getElementById("output-area").innerText = "whatever the AI/API says/sends back";
+}
+
 
