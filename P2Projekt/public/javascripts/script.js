dec35184129c973d9c53c1adad6e9ce5555b66ae
Imar <116631084+MrButtNugget@users.noreply.github.com>
2023-04-18 14:40:33 +0200

    Added a CPR remover
    removes multiple instances of 10 digit numbers both with and without a "-"

--- P2Projekt/public/javascripts/script.js
+++ P2Projekt/public/javascripts/script.js
@@ -2,22 +2,29 @@ function Helloworld(){
     alert("Helloworld");
 }
 
-function outputText() {
-    const inputText = document.getElementById("input-text").value;
-    document.getElementById("output-area").innerText = inputText + " but it has been translated";
+function inputTextNoCPR(){
+    let inputText = document.getElementById("input-text").value;
+    const CPRpattern = /\d{10}|\d{6}-\d{4}/;
+    while (CPRpattern.test(inputText)){
+        const outputTextNoCPR = inputText.replace(CPRpattern, "XXXXXX-XXXX");
+        inputText = outputTextNoCPR;
+    }
+    return inputText;
+    //document.getElementById("output-area").innerText = inputText;
+    
 }
 
 function TextPromptToAI(prompt) {
-    const AIprompt = prompt;
-    const inputText = document.getElementById("input-text").value;
+    const AIprompt = prompt;    
+    const inputText = inputTextNoCPR();
     const ToAIText = AIprompt + inputText;
 
-    // add API stuff here    
-
+    // add API stuff here 
 
 
     //prints the returned text from the API in a textbox on the website
-    document.getElementById("output-area").innerText = "whatever the AI/API says/sends back";
+    document.getElementById("output-area").innerText = inputText; //change this accordingly.
 }
+
 
 
