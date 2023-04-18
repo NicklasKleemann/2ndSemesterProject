33fd64eee85099fc8380b824d3e7e714ac8cc0be
Lasse Drongesen <Lassemgp@gmail.com>
2023-04-18 13:56:49 +0200

    Copy button and paste button added to boxes, review section, google maps.
    Further development on the application with some neat new features.

--- P2Projekt/routes/index.js
+++ P2Projekt/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  res.render('index', { title: 'EHR JOURNAL HALLØJ' });
+  res.render('index', { title: 'Electronic Health Record Translaton' });
 });
 
 module.exports = router;
