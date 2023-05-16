b440e3066af507ed67584ba64f03351c3be96edc
Lasse Drongesen <Lassemgp@gmail.com>
2023-05-16 15:21:54 +0200

    Moved GPT request to backend
    lavet en post-request til backend hvor API kaldet til openAI foregår og sender et respone object tilbage til frontend som vi så extracter beskeden fra

--- P2Projekt/app.js
+++ P2Projekt/app.js
@@ -16,12 +16,6 @@ var captchaRouter = require('./routes/captcha');
 
 var app = express();
 
-app.get('/api_key', (req, res) => {
-  const API_KEY = process.env.OPENAI_API_KEY;
-  res.send(API_KEY);
-})
-
-
 app.use(express.json());
 
 app.post('/gethomepage', async (req, res) => {
@@ -64,6 +58,84 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', captchaRouter);
 app.use('/Home', indexRouter);
 app.use('/FAQ', usersRouter);
+
+app.post('/gpt3_5/completion', async (req, res) => {
+  const API_KEY = process.env.OPENAI_API_KEY
+  console.log("Hello im in the handler")
+  const record = req.body.record || "";
+  console.log(record)
+  let systemText = `You're a GPT-based bot designed to enhance the readability and comprehensibility of medical records. The bot takes unstructured medical records as input and produces a refined version that is easier to read and understand. The bot's primary goal is to make medical records more accessible and user-friendly, improving patient outcomes and facilitating communication between healthcare providers. Furthermore if the input medical record does not appear to actually be a medical record, then request user to provide a proper medical record. Also the response has to be in the same language as the medical record provided.`
+  let promt = `Write a detailed explanation of the following medical record for uneducated people. This description must also explain medical methods, techniques, operations, or other treatment options and treatment  courses mentioned in the record. Medical Record: "${record}". The response has to be in the same language as the medical record.`
+
+  if (!API_KEY) {
+      res.status(500).json({
+          error: {
+              message: "OpenAI API key not configured correctly"
+          }
+      });
+      return;
+  }
+
+  if (record.trim().length === 0) {
+      res.status(400).json({
+          error: {
+              message: "Please enter a valid health record"
+          }
+      })
+      return;
+  }
+
+  try {
+      const messages = [];
+
+      // add the system message
+      const systemMessage = {
+          role: "system",
+          content: systemText
+          };
+          if (systemText.length > 0) {
+              messages.push(systemMessage);
+          }
+
+          // add the user message
+          const inputMessage = {
+          role: "user",
+          content: promt
+          };
+          if (record.length > 0) {
+              messages.push(inputMessage);
+          }
+
+          const response = await fetch('https://api.openai.com/v1/chat/completions', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': 'Bearer ' + API_KEY,
+              },
+              body: JSON.stringify({
+                  "model": "gpt-3.5-turbo",
+                  "messages": messages,
+                  "temperature": 0
+              })
+          });
+          const responseJSON = await response.json()
+        
+          res.status(200).json({ result: responseJSON });
+  } catch(error) {
+      // Consider adjusting the error handling logic for your use case
+      if (error.response) {
+        console.error(error.response.status, error.response.data);
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        console.error(`Error with OpenAI API request: ${error.message}`);
+        res.status(500).json({
+          error: {
+            message: 'An error occurred during your request.',
+          }
+        });
+      }
+  }
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- P2Projekt/public/javascripts/script.js
+++ P2Projekt/public/javascripts/script.js
@@ -15,79 +15,34 @@ function inputTextNoCPR(){
 // Kør localStorage.setItem("openAI", "YOUR_OPENAI_API_KEY"); i developer consolen for at sætte API-keyen
 /*  const API_KEY = localStorage.getItem("openAI"); */
 
-let API_KEY;
-
-async function getAPIKey() {
-    // Send a GET request to the server to get the API key
-    const response = await fetch("/api_key");
-    
-    // Parse the response text as a string
-    const apiKey = await response.text();
-    
-    // Return the API key
-    return apiKey;
-}
-
-async function init() {
-    API_KEY = await getAPIKey();
-}
-init();
 
 async function chatGPT_API_Completions() {
     //Cache DOM elements to avoid unnecessary DOM traversals
     let responseElem = document.getElementById("output-area")
     let inputText = inputTextNoCPR()
     responseElem.innerText = "Working..."
-    let systemText = `You're a GPT-based bot designed to enhance the readability and comprehensibility of medical records. The bot takes unstructured medical records as input and produces a refined version that is easier to read and understand. The bot's primary goal is to make medical records more accessible and user-friendly, improving patient outcomes and facilitating communication between healthcare providers. Furthermore if the input medical record does not appear to actually be a medical record, then request user to provide a proper medical record. Also the response has to be in the same language as the medical record provided.`
-    let promt = `Write a detailed explanation of the following medical record for uneducated people. This description must also explain medical methods, techniques, operations, or other treatment options and treatment  courses mentioned in the record. Medical Record: "${inputText}". The response has to be in the same language as the medical record.`
 
-    if (inputText) {
-        try {
-
-            const messages = [];
-
-            // add the system message
-            const systemMessage = {
-            role: "system",
-            content: systemText
-            };
-            if (systemText.length > 0) {
-                messages.push(systemMessage);
-            }
-
-            // add the user message
-            const inputMessage = {
-            role: "user",
-            content: promt
-            };
-            if (inputText.length > 0) {
-                messages.push(inputMessage);
-            }
-
-            const response = await fetch('https://api.openai.com/v1/chat/completions', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + API_KEY,
-                },
-                body: JSON.stringify({
-                    "model": "gpt-3.5-turbo",
-                    "messages": messages,
-                    "temperature": 0
-                })
-            });
-
-            if (!response.ok) {
-                console.error("HTTP ERROR: " + response.status + "\n" + response.statusText);
-                typeSentence("HTTP ERROR: " + response.status, responseElem);
-            } else {
-                const data = await response.json();
-                typeSentence(createResponse(data), responseElem, data, true);
-            }
-        } catch (error) {
-            console.error("ERROR: " + error);
+    try {
+        const response = await fetch("/gpt3_5/completion", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ record: inputText }),
+        });
+        const data = await response.json()
+        if (response.status !== 200) {
+            throw data.error || new Error(`Request failled with status ${response.status}`)
         }
-    }}
+        
+        typeSentence(createResponse(data.result), responseElem, data, true);
+    } catch (error) {
+            console.error(error);
+            alert(error.message);
+    }          
+}
+
+
 
 
 
