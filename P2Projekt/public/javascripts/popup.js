88f3afaf51aa4defe3e9ccdd5a05088061a97503
Lasse Drongesen <Lassemgp@gmail.com>
2023-05-19 12:13:59 +0200

    Popup (Emilie), with minor changes

---
+++ P2Projekt/public/javascripts/popup.js
@@ -0,0 +1,21 @@
+
+if (sessionStorage.getItem('loadPopup') !== 'false') {
+    document.addEventListener("DOMContentLoaded", () => {
+    
+        const popup = document.querySelector('.popup');
+        const x = document.querySelector('.popup-content h1')
+
+        window.addEventListener('load', () => {
+            popup.classList.add('showPopup');
+            popup.childNodes[1].classList.add('showPopup');
+        })
+        x.addEventListener('click', () => {
+            popup.classList.remove('showPopup');
+            popup.childNodes[1].classList.remove('showPopup');
+        })
+        sessionStorage.setItem('loadPopup', 'false');
+    })
+}
+
+    
+    
